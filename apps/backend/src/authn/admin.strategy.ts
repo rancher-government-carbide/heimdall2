d9cd73606da6968015ebd31f5b1310ccfed0b12b
Adam Toy <adam.toy@rancherfederal.com>
2023-01-26 14:49:33 -0500

    update

---
+++ apps/backend/src/authn/admin.strategy.ts
@@ -0,0 +1,49 @@
+import {IUser} from '@heimdall/interfaces';
+import {HttpException, Injectable, UnauthorizedException} from '@nestjs/common';
+import {PassportStrategy} from '@nestjs/passport';
+import * as jwt from 'jsonwebtoken';
+import {ExtractJwt, Strategy} from 'passport-jwt';
+import {ConfigService} from '../config/config.service';
+import {UsersService} from '../users/users.service';
+
+const ADMIN_EMAIL = 'admin@heimdall.local'
+
+@Injectable()
+export class AdminStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKeyProvider: async (
+        _request: Express.Request,
+        jwtToken: string,
+        done: (exception: null | HttpException, secret?: string) => unknown
+      ) => {
+        const decodedToken = jwt.decode(jwtToken) as {
+          sub: string;
+        };
+        try {
+          const user = await usersService.findByEmail(ADMIN_EMAIL);
+          done(null, configService.get('JWT_SECRET') + user.jwtSecret);
+        } catch {
+          done(
+            new UnauthorizedException(
+              'An exception occurred while validating your session'
+            )
+          );
+        }
+      },
+      ignoreExpiration: false
+    });
+  }
+
+  async validate(payload: {
+    sub: string;
+    email: string;
+    role: string;
+  }): Promise<IUser> {
+    return this.usersService.findByEmail(ADMIN_EMAIL);
+  }
+}
